73198905e469ff50f6e89e376d698761e70fc3c9
SergeRadinovich <serge@edgeworx.io>
2019-08-30 16:47:54 +1200

    Add kog role to operator yaml

--- pkg/controller/kog/install/k8s_microservices.go
+++ pkg/controller/kog/install/k8s_microservices.go
@@ -15,10 +15,8 @@ package install
 
 import (
 	"github.com/eclipse-iofog/iofogctl/pkg/iofog"
-	"github.com/eclipse-iofog/iofogctl/pkg/util"
 	"k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"strings"
 )
 
 type microservice struct {
