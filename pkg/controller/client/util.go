84f1894dc4da54b4a98bde9a3b8f32d2ad327ca1
SergeRadinovich <serge@edgeworx.io>
2019-09-01 21:54:52 +1200

    Add refactored k8s client

---
+++ pkg/controller/client/util.go
@@ -0,0 +1,26 @@
+/*
+ *  *******************************************************************************
+ *  * Copyright (c) 2019 Edgeworx, Inc.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v. 2.0 which is available at
+ *  * http://www.eclipse.org/legal/epl-2.0
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *  *******************************************************************************
+ *
+ */
+
+package client
+
+import (
+	"strings"
+)
+
+func before(input string, substr string) string {
+	pos := strings.Index(input, substr)
+	if pos == -1 {
+		return input
+	}
+	return input[0:pos]
+}
---
+++ pkg/controller/client/wait.go
@@ -0,0 +1,106 @@
+/*
+ *  *******************************************************************************
+ *  * Copyright (c) 2019 Edgeworx, Inc.
+ *  *
+ *  * This program and the accompanying materials are made available under the
+ *  * terms of the Eclipse Public License v. 2.0 which is available at
+ *  * http://www.eclipse.org/legal/epl-2.0
+ *  *
+ *  * SPDX-License-Identifier: EPL-2.0
+ *  *******************************************************************************
+ *
+ */
+
+package client
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (cl *Client) WaitForService(namespace, name string) (ip string, err error) {
+	// Get watch handler to observe changes to services
+	watch, err := cl.CoreV1().Services(namespace).Watch(metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	// Wait for Services to have IPs allocated
+	for event := range watch.ResultChan() {
+		svc, ok := event.Object.(*v1.Service)
+		if !ok {
+			err = errors.New("Could not wait for service " + namespace + "/" + name)
+			return
+		}
+
+		// Ignore irrelevant service events
+		if svc.Name != name {
+			continue
+		}
+		// Loadbalancer must be ready
+		if len(svc.Status.LoadBalancer.Ingress) == 0 {
+			continue
+		}
+
+		ip = svc.Status.LoadBalancer.Ingress[0].IP
+		watch.Stop()
+	}
+
+	return
+}
+
+func (cl *Client) WaitForPod(namespace, name string) error {
+	// Get watch handler to observe changes to pods
+	watch, err := cl.CoreV1().Pods(namespace).Watch(metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	// Wait for pod events
+	for event := range watch.ResultChan() {
+		// Get the pod
+		pod, ok := event.Object.(*v1.Pod)
+		if !ok {
+			return errors.New("Failed to wait for pod " + namespace + "/" + name)
+		}
+		// Check pod is in running state
+		if before(pod.Name, "-") != name {
+			continue
+		}
+
+		if pod.Status.Phase == "Running" {
+			ready := true
+			for _, cond := range pod.Status.Conditions {
+				if cond.Status != "True" {
+					ready = false
+					break
+				}
+			}
+			if ready {
+				watch.Stop()
+			}
+		}
+	}
+	return nil
+}
+
+func (cl *Client) WaitForPodTerminate(namespace, name string) error {
+	terminating := false
+	for !terminating {
+		_, err := cl.CoreV1().Pods(namespace).Get(name, metav1.GetOptions{})
+		if err != nil {
+			terminating = strings.Contains(err.Error(), "not found")
+			if !terminating {
+				return err
+			}
+		}
+		if !terminating {
+			time.Sleep(time.Millisecond * 500)
+		}
+	}
+	return nil
+}
