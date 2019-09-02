3a9a4a2c2a9c33486e6502ddc88c4df02fd95337
SergeRadinovich <serge@edgeworx.io>
2019-09-02 22:08:02 +1200

    Remove iokog api dir

--- pkg/apis/iokog/group.go
+++
@@ -1,6 +0,0 @@
-// Package iokog contains iokog API versions.
-//
-// This file ensures Go source parsers acknowledge the iokog package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package iokog
