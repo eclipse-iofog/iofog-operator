e136a20404e9d34a115372587f14f4a3c76a7a10
SergeRadinovich <serge@edgeworx.io>
2020-03-16 14:03:34 +1300

    Fix no scheme error

--- pkg/apis/add.go
+++ pkg/apis/add.go
@@ -1,10 +1,12 @@
 package apis
 
 import (
-	"github.com/eclipse-iofog/iofog-operator/v2/pkg/apis/iofog"
+	v1 "github.com/eclipse-iofog/iofog-operator/pkg/apis/iofog/v1"
+	v2 "github.com/eclipse-iofog/iofog-operator/v2/pkg/apis/iofog"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
-	AddToSchemes = append(AddToSchemes, iofog.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes, v1.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes, v2.SchemeBuilder.AddToScheme)
 }
