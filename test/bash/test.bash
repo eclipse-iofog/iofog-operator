9e04469b49815f883534fc42df723c24d6d8fc30
Serge Radinovich <serge@edgeworx.io>
2021-01-19 22:14:48 +1300

    Fix operator deploy test

--- test/bash/test.bash
+++ test/bash/test.bash
@@ -32,11 +32,31 @@ function testDeployOperator() {
   kctl apply -f config/operator/rbac.yaml
   kctl apply -f config/operator/deployment.yaml
   kctl wait --for=condition=Ready pods -l name=iofog-operator --timeout 1m
-  kctl logs -l name=iofog-operator | grep "INFO	setup	starting manager"
-  kctl logs -l name=iofog-operator | grep "successfully acquired lease"
-  kctl logs -l name=iofog-operator | grep 'Starting Controller	{"reconcilerGroup": "iofog.org", "reconcilerKind": "Application", "controller": "application"}'
-  kctl logs -l name=iofog-operator | grep 'Starting workers	{"reconcilerGroup": "iofog.org", "reconcilerKind": "Application", "controller": "application", "worker count": 1}'
-  kctl logs -l name=iofog-operator | grep 'Starting Controller	{"reconcilerGroup": "iofog.org", "reconcilerKind": "ControlPlane", "controller": "controlplane"}'
-  kctl logs -l name=iofog-operator | grep 'Starting workers	{"reconcilerGroup": "iofog.org", "reconcilerKind": "ControlPlane", "controller": "controlplane", "worker count": 1}'
+
+  local TIMEOUT=30
+  local SECS=0
+  local STATUS=1
+  local TXTS=(
+    "successfully acquired lease"
+    'Starting Controller	{"reconcilerGroup": "iofog.org", "reconcilerKind": "Application", "controller": "application"}'
+    'Starting workers	{"reconcilerGroup": "iofog.org", "reconcilerKind": "Application", "controller": "application", "worker count": 1}'
+    'Starting Controller	{"reconcilerGroup": "iofog.org", "reconcilerKind": "ControlPlane", "controller": "controlplane"}'
+    'Starting workers	{"reconcilerGroup": "iofog.org", "reconcilerKind": "ControlPlane", "controller": "controlplane", "worker count": 1}'
+  )
+  while [ "$SECS" -lt "$TIMEOUT" ] && [ "$STATUS" -ne 0 ]; do
+    STATUS=0
+    local LOGS=$(kctl logs -l name=iofog-operator)
+    for TXT in "${TXTS[@]}"; do
+      local FOUND=$(echo "$LOGS" | grep "$TXT")
+      if [ -z "$FOUND" ]; then
+        STATUS=1
+      fi
+      log $STATUS
+    done
+    let "SECS=SECS+1"
+    sleep 1
+  done
+  [ "$STATUS" -eq 0 ]
+  [ "$SECS" -lt "$TIMEOUT" ]
   stopTest
 }
