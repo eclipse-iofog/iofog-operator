a3b01e081ec8678e22cfbb959c1cddb5c30195c6
sergerad <serge.radinovich@gmail.com>
2021-09-27 18:49:56 +1300

    Create controlplane CRD in tests

--- test/bash/test.bash
+++ test/bash/test.bash
@@ -20,6 +20,7 @@ function testDeleteNamespace() {
 function testCreateCRD() {
   startTest
   kctl apply -f config/crd/bases/iofog.org_applications.yaml
+  kctl apply -f config/crd/bases/iofog.org_controlplanes.yaml
   kctl get crds | grep "controlplanes\.iofog\.org"
   kctl get crds | grep "apps\.iofog\.org"
   stopTest
